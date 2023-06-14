505a5b02d627143b66f5e80e7ffbe14f1d6b5180
pscon <ekunolapaul@gmail.com>
2023-06-14 21:54:24 +0100

    Feat: developed the header for resume informations and added background

--- src/layouts/resumeHeader-Footer/ResumeInfoHeader.jsx
+++ src/layouts/resumeHeader-Footer/ResumeInfoHeader.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaArrowRight } from "react-icons/fa";
 import { Link, useLocation } from "react-router-dom";
 import { konectinIcon } from "../../assets";
 import "../header/header.css";
 
-function ResumeHeader() {
+function ResumeInfoHeader() {
   const [offset, setOffset] = useState({
     prevScrollpos: window.pageYOffset,
     visible: true,
   });
 
   const [isOpen, setToggle] = useState(false);
-  const links = [
+  const navLinks = [
     { name: "Home", link: "/" },
     { name: "Internships", link: "/internship" },
     { name: "Resume Builder", link: "/resume" },
@@ -19,6 +19,29 @@ function ResumeHeader() {
     { name: "About Us", link: "/about" },
   ];
 
+  const links = [
+    { path: "/resume/builder", text: "BASIC INFO", no: "1" },
+    {
+      path: "/resume/builder/employment-experience",
+      text: "WORK HISTORY",
+      icon: <FaArrowRight size="1rem" />,
+      no: "2",
+    },
+    {
+      path: "/resume/builder/education",
+      text: "EDUCATION",
+      icon: <FaArrowRight size="1rem" />,
+      no: "3",
+    },
+    {
+      path: "/resume/builder/skills",
+      text: "SKILL",
+      icon: <FaArrowRight size="1rem" />,
+      no: "4",
+    },
+    { path: "/bio", text: "BIO", icon: <FaArrowRight size="1rem" />, no: "5" },
+    { path: "/resume/builder/download", text: "FINALIZE", no: "6" },
+  ];
   const { pathname } = useLocation();
 
   const handleScroll = () => {
@@ -76,22 +99,41 @@ function ResumeHeader() {
             <FaBars size="1.5rem" color={offset?.darken ? "#fff" : "#332a66"} />
           )}
         </nav>
-        <nav className="hidden gap-8 transistion-all md:flex flex-row text-sm">
+
+        <nav className="hidden gap-1 transistion-all md:flex flex-row text-sm">
           {links.map((link, index) => (
             <Link
-              className={
-                (link.link === "/blog" && pathname.split("/")[1] === "blog") ||
-                link.link === pathname
-                  ? "py-1 border-b border-secondary-600"
-                  : "py-1 hover:border-b border-secondary-600"
-              }
               key={index}
-              to={link.link === "/blog" ? "/blog/all" : link.link}
+              exact
+              to={link.path}
+              activeClassName="text-red-500"
+              className={`flex items-center  hover:text-red-500 px-3 py-2 rounded-md text-sm font-medium ${
+                (link.path === "/resume/builder/employment-experience" &&
+                  pathname.includes("/resume/builder/employment-experience")) ||
+                link.path === pathname
+                  ? "text-red-500 "
+                  : "text-red-300"
+              }`}
             >
-              {link.name}
+              {" "}
+              <span
+                className={`circleOrange border border-red-300 text-red-300 mr-1 text-sm font-medium ${
+                  (link.path === "/resume/builder/employment-experience" &&
+                    pathname.includes(
+                      "/resume/builder/employment-experience"
+                    )) ||
+                  link.path === pathname
+                    ? "text-red-500 "
+                    : "text-red-300"
+                }`}
+              >
+                {link.no}
+              </span>
+              {link.text}
             </Link>
           ))}
         </nav>
+
         {/* Mobile View  */}
         <nav
           className={
@@ -100,7 +142,7 @@ function ResumeHeader() {
               : "hidden"
           }
         >
-          {links.map((link, index) => (
+          {navLinks.map((link, index) => (
             <Link
               className={
                 (link.link === "/blog" && pathname.split("/")[1] === "blog") ||
@@ -116,21 +158,10 @@ function ResumeHeader() {
             </Link>
           ))}
         </nav>
-        <nav className="hidden lg:block">
-          <Link
-            to="/login"
-            className={`w-full text-sm px-6 py-2 text-black-500 border-secondary-500 border rounded-sm ${
-              offset.darken
-                ? "hover:text-neutral-100 hover:bg-white"
-                : "hover:text-white hover:bg-secondary-500"
-            } transistion duration-500`}
-          >
-            Log In
-          </Link>
-        </nav>{" "}
+        <nav className="hidden lg:block"></nav>
       </nav>
     </header>
   );
 }
 
-export default ResumeHeader;
+export default ResumeInfoHeader;
--- src/pages/ResumeRoutes/index.jsx
+++ src/pages/ResumeRoutes/index.jsx
@@ -1,13 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import ScrollToTop from "../../components/scrollToTop";
 import ResumeFooter from "../../layouts/resumeHeader-Footer/ResumeFooter";
 import ResumeHeader from "../../layouts/resumeHeader-Footer/ResumeHeader";
+import ResumeInfoHeader from "../../layouts/resumeHeader-Footer/ResumeInfoHeader";
 
 function ResumeRoutes() {
+  const { pathname } = useLocation();
+
   return (
     <>
       <ScrollToTop />
-      <ResumeHeader />
+      {pathname === "/resume/builder" ||
+      pathname === "/resume/builder/employment-experience" ||
+      pathname === "/resume/builder/education" ||
+      pathname === "/resume/builder/skills" ||
+      pathname === "/resume/builder/download" ? (
+        <ResumeInfoHeader />
+      ) : (
+        <ResumeHeader />
+      )}
       <Outlet />
       <ResumeFooter />
     </>
