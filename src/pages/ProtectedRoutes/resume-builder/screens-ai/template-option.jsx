cf420757ea62f48c2b80e7b89edecd4d6e5b9712
adeyanjufawaz <fawaz>
2023-07-12 20:30:25 +0100

    new brach

--- src/pages/ProtectedRoutes/resume-builder/screens-ai/template-option.jsx
+++ src/pages/ProtectedRoutes/resume-builder/screens-ai/template-option.jsx
@@ -57,31 +57,12 @@ const TemplateOption = ({ sectionName }) => {
       <h3 className="text-xl lg:text-2xl font-bold capitalize">
         {sectionName}
       </h3>
-<<<<<<< Updated upstream
-      <div className="lg:w-[800px] md:w-[550px] w-[270px] lg:h-[400px]">
-        <Swiper spaceBetween={20} slidesPerView={2.7}>
-=======
       <div className="lg:w-[900px] md:w-[550px] w-[300px] lg:h-[450px] overflow-x-auto flex items-center justify-center gap-10 lg:px-8">
         <Swiper spaceBetween={30} slidesPerView={2.7}>
->>>>>>> Stashed changes
           {template_images
             .filter((record) => record.category === sectionName)
             .map((item, index) => {
               return (
-<<<<<<< Updated upstream
-                <SwiperSlide
-                  key={index}
-                  className="cursor-pointer flex justify-center items-center text-center"
-                >
-                  <div
-                    className={`
-                                ${
-                                  templateData.selectedTemplate ===
-                                  `${sectionName}_template_${index + 1}`
-                                    ? "absolute w-full h-full top-0 bg-neutral-100 bg-opacity-60"
-                                    : "-top-full"
-                                } left-0 duration-500 flex items-center justify-center`}
-=======
                 <SwiperSlide className="cursor-pointer ml-2 flex justify-center items-center text-center">
                   <div
                     className={`
@@ -91,7 +72,6 @@ const TemplateOption = ({ sectionName }) => {
               ? "absolute w-full h-full top-0 bg-neutral-100 bg-opacity-60"
               : "-top-full"
           } left-0 duration-500 flex items-center justify-center`}
->>>>>>> Stashed changes
                   >
                     {templateData.selectedTemplate ===
                       `${sectionName}_template_${index + 1}` && (
