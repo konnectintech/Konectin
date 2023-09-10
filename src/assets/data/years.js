51ae6d5be92b874f06964f6801439e3cc36fdb14
Ajayi Michael <ajayimichael150@gmail.com>
2023-09-10 18:31:28 +0100

    pic

--- src/assets/data/years.js
+++
@@ -1,5 +0,0 @@
-const currentYear = new Date().getFullYear();
-export const years = [];
-for (let year = currentYear; year >= 1905; year--) {
-  years.push({ id: year.toString(), label: year.toString() });
-}
--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { backgroundVideo } from "../../assets/data";
 
 function ErrorPage() {
   return (
     <section className="flex flex-col justify-center items-center py-24">
-      <section className="relative">
+      <section className="w-full h-full relative">
         <video
           className="absolute object-cover w-full h-full left-0 top-0"
-          src=""
+          src={backgroundVideo}
           autoPlay
           loop
           muted
         ></video>
-        <p className="w-full h-full text-[100px] font-black tracking-[2rem] mix-blend-multiply">
+        <h1 className="w-full h-full text-[100px] font-black tracking-[2rem] mix-blend-multiply">
           OOPS!
-        </p>
+        </h1>
       </section>
       <div>
         <h2 className="text-2xl md:text-4xl">404 - PAGE NOT FOUND</h2>
@@ -22,8 +23,11 @@ function ErrorPage() {
           The page you are looking for might have been remoeved <br /> had its
           name changed or is temporarily unavailable.
         </p>
-        <Link className="text-white bg-secondary-400 shadow-sm shadow-secondary-600 py-3 px-6">
-          GO TO HOMEPAGE
+        <Link
+          to="/"
+          className="text-white hover:bg-secondary-500 bg-secondary-400 shadow-sm shadow-secondary-600 py-3 px-6"
+        >
+          GO TO HOME PAGE
         </Link>
       </div>
     </section>
