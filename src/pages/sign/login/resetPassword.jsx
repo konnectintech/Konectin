d279b18c89fea50c4750c5374a9c4b604ab57e0a
Ajayi Michael <ajayimichael150@gmail.com>
2023-09-12 09:03:56 +0100

    reset password design

--- src/pages/sign/login/resetPassword.jsx
+++ src/pages/sign/login/resetPassword.jsx
@@ -6,7 +6,7 @@ import { useAuth } from "../../../middleware/auth";
 import Preloader from "../../../components/preloader";
 import { InteractionRequiredAuthError } from "@azure/msal-browser";
 import { CustomButton } from "../../../components/button";
-import { konectinIcon } from "../../../assets";
+import { forgotPasswordImage, konectinIcon } from "../../../assets";
 import { ErrorModal, NotifyModal } from "../../../components/form/modal";
 import * as MdIcons from "react-icons/md";
 import axios from "axios";
@@ -98,8 +98,8 @@ function ResetPassword() {
                 Password Reset Successfully 🎉
               </>
             ),
-            p1: "You have successfully reset your password.",
-            p2: "Your new password has been set, and you can now log in with confidence. Please make sure to keep your new password secure and do not share it with anyone.",
+            p1: "Congrats! Your password has been changed successfully.",
+            p2: "Your new password has been set, and you can now log in with confidence. Please make sure to keep your new password secure and do not share it with anyone.😎",
             button: "Continue",
           };
           setState(newState);
@@ -177,10 +177,10 @@ function ResetPassword() {
   };
 
   return (
-    <div className=" bg-neutral-800 auth-body flex items-center w-full sm:min-w-[500px] flex-1">
-      <div className="p-6  max-w-[600px] min-h-[80vh] lg:p-0 mx-auto flex flex-col gap-4 items-start justify-center">
-        {isloading && <Preloader />}
-        {!isCodeValid ? (
+    <div className="bg-[#F5F5F5] flex items-center w-full sm:min-w-[500px] flex-1">
+      {!isCodeValid ? (
+        <div className="p-6 max-w-[600px] min-h-[80vh] lg:p-0 mx-auto flex flex-col gap-4 items-start justify-center">
+          {isloading && <Preloader />}
           <section className=" max-w-md mx-auto text-center space-y-10">
             <Link
               to="/"
@@ -241,127 +241,150 @@ function ResetPassword() {
               </div>
             </div>
           </section>
-        ) : (
-          <>
-            <div>
-              <h1 className="text-4xl">Create New Password...</h1>
-              <p>Let's help you get your account back</p>
-            </div>
+        </div>
+      ) : (
+        <div className="mx-auto max-w-screen-xl flex flex-col md:flex-row min-h-screen gap-8 lg:gap-16 justify-between lg:px-32">
+          {isloading && <Preloader />}
+          <div className="hidden h-screen md:w-11/12 md:block relative lg:block">
+            <img
+              className="w-full h-full"
+              src={forgotPasswordImage}
+              alt="sign in background"
+            />
+          </div>
 
-            <div className="w-full flex flex-col items-stretch text-xs md:text-sm">
-              <form
-                onSubmit={handleSubmit}
-                className="w-full flex flex-col mt-2 items-stretch text-xs md:text-sm gap-6"
-              >
-                {errorMessage && <ErrorModal text={errorMessage} />}
-                <div className="flex flex-col gap-4">
-                  <fieldset className="border border-[#DC7957] rounded-md cursor-pointer relative">
-                    <legend className="ml-4 px-1">New Password</legend>
-                    <input
-                      className="w-full text-xs bg-transparent border-0 outline-0 px-4 pt-2 pb-3"
-                      id={"password"}
-                      name={"password"}
-                      type={passType["password"]}
-                      placeholder={"Minimum of 8 characters"}
-                      value={password}
-                      onChange={(event) => setPassword(event.target.value)}
-                      onInput={(event) => setPassword(event.target.value)}
-                    />
-                    <label
-                      onClick={() => togglePassType("password")}
-                      className="cursor-pointer"
-                      htmlFor={"password"}
-                    >
-                      {passType["password"] === "password" ? (
-                        <MdIcons.MdLockOpen
-                          className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
-                          color="#242424"
-                          size="1.5rem"
-                        />
-                      ) : (
-                        <MdIcons.MdLock
-                          className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
-                          color="#242424"
-                          size="1.5rem"
-                        />
-                      )}
-                    </label>
-                  </fieldset>
-
-                  <fieldset className="border border-[#DC7957] rounded-md cursor-pointer relative">
-                    <legend className="ml-4 px-1">Confirm Password</legend>
-                    <input
-                      className="w-full text-xs bg-transparent border-0 outline-0 px-4 pt-2 pb-3"
-                      id={"confirmPassword"}
-                      name={"confirmPassword"}
-                      type={passType["confirm_password"]}
-                      placeholder={"Minimum of 8 characters"}
-                      value={confirmPassword}
-                      onChange={(event) =>
-                        setConfirmPassword(event.target.value)
-                      }
-                      onInput={(event) =>
-                        setConfirmPassword(event.target.value)
-                      }
-                    />
-                    <label
-                      onClick={() => togglePassType("confirm_password")}
-                      className="cursor-pointer"
-                      htmlFor={"confirmPassword"}
-                    >
-                      {passType["confirm_password"] === "password" ? (
-                        <MdIcons.MdLockOpen
-                          className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
-                          color="#242424"
-                          size="1.5rem"
-                        />
-                      ) : (
-                        <MdIcons.MdLock
-                          className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
-                          color="#242424"
-                          size="1.5rem"
-                        />
-                      )}
-                    </label>
-                  </fieldset>
-                  {error && (
-                    <p className="text-start w-full text-red-500 mt-1 cursor-text bg-transparent">
-                      {error}
-                    </p>
-                  )}
-                </div>
-                <div className="text-center flex flex-col gap-2">
-                  <CustomButton
-                    type="submit"
-                    disabled={
-                      !password ||
-                      !confirmPassword ||
-                      password !== confirmPassword
-                    }
-                    primary={true}
-                    colorType="primary"
-                  >
-                    Reset Password
-                  </CustomButton>
-                </div>
-              </form>
-            </div>
-            {state.header && (
-              <div className="fixed top-0 left-0 w-full h-full z-50 bg-neutral-500 flex items-center justify-center">
-                <NotifyModal
-                  error={state.error}
-                  header={state.header}
-                  paragraph1={state.p1}
-                  paragraph2={state.p2}
-                  click={() => navigate("/login")}
-                >
-                  {state.button}
-                </NotifyModal>
+          <div className="items-start md:min-w-[250px] lg:w-1/4 lg:min-w-[500px] w-full sm:min-w-[400px]">
+            <div className="p-6 lg:pr-16 max-w-[600px] min-h-[80vh] lg:p-0 mx-auto flex flex-col gap-4 items-start justify-center">
+              <div className="md:hidden">
+                <img
+                  className="w-full h-full"
+                  src={konectinIcon}
+                  alt="sign in background"
+                />
               </div>
-            )}
-          </>
-        )}
-      </div>
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold">Change Password</h1>
+                <p>Let's help you get your account back</p>
+              </div>
+
+              <div className="w-full flex flex-col items-stretch text-xs md:text-sm">
+                <form
+                  onSubmit={handleSubmit}
+                  className="w-full flex flex-col mt-2 items-stretch text-xs md:text-sm gap-6"
+                >
+                  {errorMessage && <ErrorModal text={errorMessage} />}
+                  <div className="flex flex-col gap-4">
+                    <fieldset className="border border-[#DC7957] rounded-md cursor-pointer relative">
+                      <legend className="ml-4 px-1">New Password</legend>
+                      <input
+                        className="w-full text-xs bg-transparent border-0 outline-0 px-4 pt-2 pb-3"
+                        id={"password"}
+                        name={"password"}
+                        type={passType["password"]}
+                        placeholder={"Minimum of 8 characters"}
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                        onInput={(event) => setPassword(event.target.value)}
+                      />
+                      <label
+                        onClick={() => togglePassType("password")}
+                        className="cursor-pointer"
+                        htmlFor={"password"}
+                      >
+                        {passType["password"] === "password" ? (
+                          <MdIcons.MdLockOpen
+                            className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
+                            color="#242424"
+                            size="1.5rem"
+                          />
+                        ) : (
+                          <MdIcons.MdLock
+                            className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
+                            color="#242424"
+                            size="1.5rem"
+                          />
+                        )}
+                      </label>
+                    </fieldset>
+
+                    <fieldset className="border border-[#DC7957] rounded-md cursor-pointer relative">
+                      <legend className="ml-4 px-1">Confirm Password</legend>
+                      <input
+                        className="w-full text-xs bg-transparent border-0 outline-0 px-4 pt-2 pb-3"
+                        id={"confirmPassword"}
+                        name={"confirmPassword"}
+                        type={passType["confirm_password"]}
+                        placeholder={"Minimum of 8 characters"}
+                        value={confirmPassword}
+                        onChange={(event) =>
+                          setConfirmPassword(event.target.value)
+                        }
+                        onInput={(event) =>
+                          setConfirmPassword(event.target.value)
+                        }
+                      />
+                      <label
+                        onClick={() => togglePassType("confirm_password")}
+                        className="cursor-pointer"
+                        htmlFor={"confirmPassword"}
+                      >
+                        {passType["confirm_password"] === "password" ? (
+                          <MdIcons.MdLockOpen
+                            className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
+                            color="#242424"
+                            size="1.5rem"
+                          />
+                        ) : (
+                          <MdIcons.MdLock
+                            className="absolute -translate-y-1/2 top-1/2 right-3 pb-1"
+                            color="#242424"
+                            size="1.5rem"
+                          />
+                        )}
+                      </label>
+                    </fieldset>
+                    {error && (
+                      <p className="text-start w-full text-red-500 mt-1 cursor-text bg-transparent">
+                        {error}
+                      </p>
+                    )}
+                  </div>
+                  <div className="text-center flex flex-col gap-2">
+                    <CustomButton
+                      type="submit"
+                      disabled={
+                        !password ||
+                        !confirmPassword ||
+                        password !== confirmPassword
+                      }
+                      primary={true}
+                      colorType="primary"
+                    >
+                      Reset Password
+                    </CustomButton>
+                    <CustomButton onClick={() => navigate("/login")}>
+                      Back to sign in
+                    </CustomButton>
+                  </div>
+                </form>
+              </div>
+              {state.header && (
+                <div className="fixed top-0 left-0 w-full h-full z-50 bg-neutral-500 flex items-center justify-center">
+                  <NotifyModal
+                    error={state.error}
+                    header={state.header}
+                    paragraph1={state.p1}
+                    paragraph2={state.p2}
+                    click={() => navigate("/login")}
+                  >
+                    {state.button}
+                  </NotifyModal>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
